2bf6237cb093c7326c00732f2b9f320b4a909f4c
dsabelli <dsabelli3@gmail.com>
2023-08-03 16:37:05 -0400

    add argparse functions

--- dasher.py
+++ dasher.py
@@ -1,21 +1,50 @@
 import os
 import re
+import argparse
+import datetime
 
 
 def main():
-    list_files(".")
+    today = get_today()
+
+    parser = argparse.ArgumentParser(description="Replace whitespace with dashes")
+    parser.add_argument("-d", default=today, help="prefix the date as YYMMDD", type=int)
+    parser.add_argument(
+        "-s",
+        default="-",
+        help="choose symbol to replace whitespace, default is '-'",
+        type=str,
+    )
+    parser.add_argument(
+        "-p", default=".", help="specify filename, defaults to all files", type=str
+    )
+    args = parser.parse_args()
+
+    directory = os.getcwd()
+    pattern = args.p
+    date = str(args.d)
+    separator = args.s
+
+    # Rename file(s) in the current directory, prefix a date and replace whitespace with a separator
+    rename_files(directory, pattern, date, separator)
 
 
-def list_files(dir):
+def rename_files(dir: str, pattern: str, date: str, separator: str) -> None:
     for filename in os.listdir(dir):
-        if re.match(r"^.+\s.+$", filename):
-            os.rename(filename, replace_whitespace(filename))
+        newName = date + separator + filename
+        if re.match(pattern, filename):
+            os.rename(filename, replace_whitespace(newName, separator))
 
 
-def replace_whitespace(filename):
+# replace all whitespace with dashes by default
+def replace_whitespace(filename: str, repl: str) -> str:
     pattern = r"\s"
-    repl = "-"
     return re.sub(pattern, repl, filename)
+
+
+# Get todays date and format as an int formatted as YYMMDD
+def get_today() -> int:
+    return int(datetime.date.today().strftime("%y%m%d"))
 
 
 if __name__ == "__main__":
