1f1c6fac0cf3e1fecb426fcf0a5da57ea6cd2046
Dan <daniel.sabelli@westjet.com>
2023-11-20 22:54:30 -0500

    add image compression functionality

--- dasher.py
+++ dasher.py
@@ -6,9 +6,16 @@ import datetime
 import sys
 from PIL import Image
 
-
+#todo: 
+# comments for file compression functions
+# error handling
+# testing for file compression
+# folder watching
+# folder cleanup
 def main():
+    QUALITY = 95
     args = get_args()
+    size = args.c
     directory = args.d
     pattern = args.p
     date = args.t
@@ -18,6 +25,8 @@ def main():
     validate_separator(separator)
 
     rename_files(directory, pattern, date, separator)
+    resize_images(directory,separator,size,QUALITY)
+    
 
 
 # loop through files in directory. Scan for today's date with double separator
@@ -69,7 +78,16 @@ def get_args(args=None):
         type=str,
     )
     parser.add_argument(
-        "-p", default=".", help="define regex pattern, defaults to all files", type=str
+        "-p", 
+        default=".", 
+        help="define regex pattern, defaults to all files", 
+        type=str
+    ),
+    parser.add_argument(
+        "-c", 
+        default=200000, 
+        help="define image compression size as an integer, defaults to 200000", 
+        type=int
     )
     return parser.parse_args(args)
 
@@ -82,8 +100,7 @@ def get_new_name(date: str, separator: str, filename: str) -> str:
     try:
         if (
             datetime.datetime.strptime(filename[:6], "%y%m%d")
-            and filename[7] == separator
-            and filename[8] == separator
+            and filename[6:8] == separator+separator
         ):
             return filename
     except ValueError:
@@ -111,6 +128,30 @@ def validate_separator(separator: str) -> bool:
         sys.exit("Separator must be either '-' or '_'")
     return True
 
+def resize_images(dir: str, separator: str, size:int,quality:int) -> None:
+    pattern = re.compile(r"(--|__).*(\.jpg|\.jpeg)$")
+    os.chdir(dir)
+    for filename in os.scandir(dir):
+        if filename.is_file() and re.search(pattern, filename.name):
+            check_image_size(filename.name,size,quality)
+
+def check_image_size(file_path:str,size:int,quality:int)->None:
+    current_size = os.stat(file_path).st_size
+    while current_size > size or quality ==0:
+        if quality == 0:
+            os.remove(file_path)
+            print("File cannot be compressed to defined size")
+            break
+
+        compress_image(file_path,quality)
+        current_size = os.stat(file_path).st_size
+        quality-=5
+
+def compress_image(file_path:str,quality:int)->int:
+    image = Image.open(file_path)
+    image.save(file_path,"JPEG",optimize=True,quality=quality)
+    compressed_size = os.stat(file_path).st_size
+    return compressed_size
 
 if __name__ == "__main__":
     main()
