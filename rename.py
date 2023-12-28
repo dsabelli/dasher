da64243213410fe9a797a22455f50403776defac
dsabelli <dsabelli3@gmail.com>
2023-12-28 16:05:55 -0500

    refactor get_new_name for better error handling

--- rename.py
+++ rename.py
@@ -2,7 +2,7 @@ import os
 import re
 import datetime
 
-PATTERN = re.compile(r"(-|_).*(\.jpg|\.jpeg)$")
+PATTERN = re.compile(r"(\.jpg|\.jpeg)$")
 
 
 def rename_files(dir: str, pattern: str, date: str, separator: str) -> None:
@@ -30,19 +30,17 @@ def replace_whitespace(filename: str, repl: str) -> str:
 
 
 def get_new_name(date: str, separator: str, filename: str) -> str:
-    # Try to parse the first 6 characters of the filename as a date
-    try:
-        if (
-            datetime.datetime.strptime(filename[:6], "%y%m%d")
-            and filename[6] == separator
-        ):
-            # If successful, return the date followed by the rest of the filename
-            return date + filename[6:]
-    except ValueError:
-        ...
     # If the date is "0", return the original filename
     if date == "0":
         return filename
-    # Otherwise, return the date followed by the separator and the original filename
+    # Try to parse the first 6 characters of the filename as a date
     else:
-        return date + separator + filename
+        try:
+            if (
+                datetime.datetime.strptime(filename[:6], "%y%m%d")
+                and filename[6] == separator
+            ):
+                return date + filename[6:]
+        except ValueError:
+            return date + separator + filename
+        # If successful, return the date followed by the rest of the filename
