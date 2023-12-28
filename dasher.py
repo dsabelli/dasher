afe2fb8fb9e7450f8e4b1f0ba6884ed27969190c
dsabelli <dsabelli3@gmail.com>
2023-12-28 14:47:12 -0500

    add comments

--- dasher.py
+++ dasher.py
@@ -17,6 +17,8 @@ from watcher_handler import MyHandler
 # folder cleanup
 def main():
     QUALITY = 95
+
+    # get and assign command line args
     args = get_args()
     size = args.c
     directory = args.d
@@ -25,30 +27,42 @@ def main():
     date = args.t
     separator = args.s
 
+    # create the output directory if it doesn't exist
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
+    # validates the date is in YYMMDD format
     validators.validate_date(date)
+
+    # validates the separator is "-" or "_"
     validators.validate_separator(separator)
 
+    # create handlers for watcher and pass in function and function args
     rename = MyHandler(rename_files, directory, pattern, date, separator)
     resize = MyHandler(resize_images, directory, output_directory, size, QUALITY)
+
+    # create observer object and set handlers and directory to watch
     observer = Observer()
     observer.schedule(rename, path=directory, recursive=True)
     observer.schedule(resize, path=directory, recursive=True)
     observer.start()
 
     try:
+        # Enter an infinite loop
         while True:
+            # Enter an infinite loop
             time.sleep(1)
+    # If a KeyboardInterrupt (Ctrl+C, etc.) is detected, stop the observer
     except KeyboardInterrupt:
         observer.stop()
 
+    # Wait for the observer thread to finish
     observer.join()
 
 
 # Add arg defaults and return args
 def get_args(args=None):
+    SIZE = 200000
     today = datetime.date.today().strftime("%y%m%d")
     default_dir = os.getcwd()
 
@@ -82,7 +96,7 @@ def get_args(args=None):
     ),
     parser.add_argument(
         "-c",
-        default=200000,
+        default=SIZE,
         help="define image compression size as an integer, defaults to 200000",
         type=int,
     )
